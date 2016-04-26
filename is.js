143cfb22388ca6cc952db9c97c058fb45c02c751
delvedor <tommydelved@gmail.com>
2016-04-26 10:19:36 +0200

    Updated code

---
+++ is.js
@@ -0,0 +1,104 @@
+/*
+ * Project: is-electron
+ * Version: 1.0.0
+ * Author: delvedor
+ * Twitter: @delvedor
+ * License: MIT
+ * GitHub: https://github.com/delvedor/is-electron
+ */
+
+'use strict'
+
+;(() => {
+  // Constructor
+  function IsApi () {}
+
+  /*
+
+  Una soluzione potrebbe essere fare in modo che IsApi se gli vengono passati argomenti possa eseguirli
+
+  tipo:
+  is(is.mac, is.x64)
+
+  IsApi(...rest) {
+  if (rest)
+  let res
+  for (....) { eseguo le funzioni e modifico di conseguenza res}
+  return res
+}
+*/
+
+  // Checks if we are in renderer process
+  IsApi.prototype.renderer = () => {
+    return process.type === 'renderer'
+  }
+
+  // Checks if we are in main process
+  IsApi.prototype.main = () => {
+    return process.type === 'browser'
+  }
+
+  // Checks if we are under Mac OS
+  IsApi.prototype.mac = () => {
+    return process.platform === 'darwin'
+  }
+
+  // Checks if we are under Windows OS
+  IsApi.prototype.windows = () => {
+    return process.platform === 'win32'
+  }
+
+  // Checks if we are under Linux OS
+  IsApi.prototype.linux = () => {
+    return process.platform === 'linux'
+  }
+
+  // Checks if we are the processor's arch is is32
+  IsApi.prototype.ia32 = () => {
+    return process.arch === 'ia32'
+  }
+
+  // Checks if we are the processor's arch is x64
+  IsApi.prototype.x64 = () => {
+    return process.arch === 'x64'
+  }
+
+  // Checks if the env is setted to 'production'
+  IsApi.prototype.production = () => {
+    return (process.env.NODE_ENV || 'dev') === 'production'
+  }
+
+  // Checks if the env is setted to 'dev'
+  IsApi.prototype.dev = () => {
+    return (process.env.NODE_ENV || 'dev') === 'dev'
+  }
+
+  // checks if all the 'is functions' passed as arguments are true
+  IsApi.prototype.all = (...isFunctions) => {
+    if (!isFunctions.length) return
+    for (let i = 0; i < isFunctions.length; i++) {
+      if (!isFunctions[i]()) return false
+    }
+    return true
+  }
+
+  // checks if all the 'is functions' passed as arguments are false
+  IsApi.prototype.none = (...isFunctions) => {
+    if (!isFunctions.length) return
+    for (let i = 0; i < isFunctions.length; i++) {
+      if (isFunctions[i]()) return false
+    }
+    return true
+  }
+
+  // new instace
+  const is = new IsApi()
+
+  // exports
+  if (is.renderer() && window) {
+    if (!window.is) window.is = is
+    window.isElectron = is
+  } else {
+    module.exports = is
+  }
+})()
---
+++ test/test.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const path = require('path')
+const assert = require('assert')
+const electron = require('electron')
+
+const ipc = electron.ipcMain
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const is = require('../is')
+let mainWindow
+
+app.on('ready', function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 300
+  })
+  mainWindow.loadURL(path.join('file://', __dirname, '/test.html'))
+  mainWindow.openDevTools()
+  mainWindow.on('closed', function windowClosed () {
+    app.quit()
+  })
+})
+
+ipc.on('all-test-passed', (event) => {
+  assertions(() => {
+    console.log('All test passed!')
+    setTimeout(() => {
+      app.quit()
+    }, 1000)
+  })
+})
+
+function assertions (callback) {
+  assert.equal(is.main(), process.type === 'browser', 'is.main() not ok!')
+  assert.equal(is.renderer(), process.type === 'renderer', 'is.renderer() not ok!')
+
+  assert.equal(is.mac(), process.platform === 'darwin', 'is.mac() not ok!')
+  assert.equal(is.windows(), process.platform === 'win32', 'is.windows() not ok!')
+  assert.equal(is.linux(), process.platform === 'linux', 'is.linux() not ok!')
+
+  assert.equal(is.ia32(), process.arch === 'ia32', 'is.ia32() not ok!')
+  assert.equal(is.x64(), process.arch === 'x64', 'is.x64() not ok!')
+
+  assert.equal(is.production(), (process.env.NODE_ENV || 'dev') === 'production', 'is.production() not ok!')
+  assert.equal(is.dev(), (process.env.NODE_ENV || 'dev') === 'dev', 'is.dev() not ok!')
+
+  assert.equal(is.all(is.mac, is.x64), is.mac() && is.x64(), 'is.all() 1 not ok!')
+  assert.equal(is.all(is.mac, is.ia32), is.mac() && is.ia32(), 'is.all() 2 not ok!')
+  assert.equal(is.none(is.windows, is.ia32), !is.windows() && !is.ia32(), 'is.none() 1 not ok!')
+  assert.equal(is.none(is.windows, is.x64), !is.windows() && !is.x64(), 'is.none() 2 not ok!')
+
+  callback()
+}
